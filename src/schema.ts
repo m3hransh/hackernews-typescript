b9989cb7049ad771d1c1f293fc32dab3e4210d14
Mehran Shahidi <m.mehrashahidi@gmail.com>
2021-12-27 09:22:43 +0330

    initial server

--- src/schema.ts
+++ src/schema.ts
@@ -4,7 +4,7 @@ import { join } from 'path'
 export const schema = makeSchema({
   types: [],
   outputs: {
-    schema: join(__dirname, '..', 'schema.graphql'),
-    typegen: join(__dirname, '..', 'nexus-typegen.ts'),
+    schema: join(__dirname, '.', 'schema.graphql'),
+    typegen: join(__dirname, '.', 'nexus-typegen.ts'),
   },
 })
