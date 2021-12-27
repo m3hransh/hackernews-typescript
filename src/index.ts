ac8741efd08b52f8eee78f8cbbbbd568a92ba93d
Mehran Shahidi <m.mehrashahidi@gmail.com>
2021-12-27 14:50:18 +0330

    Define type Link
    - added feed query
    - added link data type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from 'apollo-server'
+
+import { ApolloServerPluginLandingPageGraphQLPlayground } 
+from 'apollo-server-core'
+import { schema } from "./schema"
 
 // 1
-import { schema } from "./schema";
 export const server = new ApolloServer({
-    schema,
-});
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+})
+
 
 const port = 3000;
 // 2
 server.listen({port}).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+  console.log(`🚀  Server ready at ${url}`)
+})
+
+
+
+
