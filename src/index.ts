862215daa2ca3c7879a0745c0f633c69cbc99817
Mehran Shahidi <m.mehrashahidi@gmail.com>
2022-01-04 17:01:21 +0100

    Refactoing
    - chekcing config

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
 import { ApolloServer } from 'apollo-server'
 
-import { ApolloServerPluginLandingPageGraphQLPlayground } 
-from 'apollo-server-core'
-import { schema } from "./schema"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import { schema } from './schema'
 
-// 1
 export const server = new ApolloServer({
   schema,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 })
 
+const port = 3000
 
-const port = 3000;
-// 2
-server.listen({port}).then(({ url }) => {
+server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
 })
-
-
-
-
