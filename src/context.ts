a749a2ac74546d860870298cd359c20ebf2701b2
Mehran Shahidi <m.mehrashahidi@gmail.com>
2022-01-16 20:48:20 +0330

    feat: decoding jwt from the http header

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,24 @@
 import { PrismaClient } from '@prisma/client'
+import { Request } from 'express'
+import { decodeAuthHeader } from './utils/auth'
 
 export const prisma = new PrismaClient()
 
 export interface Context {
   prisma: PrismaClient
+  userId?: number
 }
 
-export const context: Context = {
-  prisma,
+// Apollo server is smart enough to call this like
+// a HTTP request when is provided to its constructor
+export const context = ({ req }: {req: Request}): Context => {
+  const token = 
+    req && req.headers.authorization
+      ? decodeAuthHeader(req.headers.authorization)
+      : null
+
+  return {
+    prisma,
+    userId: token?.userId,
+  }
 }
